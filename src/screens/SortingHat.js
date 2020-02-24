0c1b8f5ce349521372e22c826e4e6abe7a637cd8
Devon McCalmon <devonmccalmon@Devons-MacBook-Pro.local>
2020-02-24 17:54:53 -0500

    set up images to render depending house

--- src/screens/SortingHat.js
+++ src/screens/SortingHat.js
@@ -13,7 +13,7 @@ class SortingHat extends Component {
 
   async getHouse() {
     try {
-       const response = await axios.get(SORTING_API)
+      const response = await axios.get(SORTING_API)
       console.log(response.data)
       this.setState({
         house: response.data
@@ -27,17 +27,73 @@ class SortingHat extends Component {
     this.getHouse()
   }
 
+  houseImage = () => {
+    if (this.state.house === 'Hufflepuff') {
+      return (
+        <div>
+          <h3>You've been by HufflePuff</h3>
+
+          <img src='https://vignette.wikia.nocookie.net/harrypotter/images/6/62/Download_%289%29.jpg/revision/latest?cb=20160729145653'
+            height='200px'
+            width='200px'
+            alt='HufflePuff' />
+
+        </div>
+      )
+    } else if (this.state.house === 'Ravenclaw') {
+      return (
+        <div>
+          <h3>You've been by RavenClaw</h3>
+
+          <img src='https://vignette.wikia.nocookie.net/harrypotter/images/2/28/Download_%284%29.jpg/revision/latest?cb=20160729145945'
+            height='200px'
+            width='200px'
+            alt='Ravenclaw' />
+
+        </div>
+      )
+    } else if (this.state.house === 'Gryffindor') {
+      return (
+        <div>
+          <h3>You've been by Gryffindor</h3>
+
+          <img src='https://vignette.wikia.nocookie.net/harrypotter/images/e/ee/Gryffindor_Crest-0.jpg/revision/latest?cb=20160729145529'
+            height='200px'
+            width='200px'
+            alt='Gryffindor' />
+
+        </div>
+      )
+    } else if (this.state.house === 'Slytherin') {
+      return (
+        <div>
+          <h3>You've been by Slytherin</h3>
+
+          <img src='https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest?cb=20160729150848'
+            height='200px'
+            width='200px'
+            alt='Slytherin' />
+
+        </div>
+      )
+    }
+  }
+
   render() {
+
+    const { house } = this.state
+    const { handleButton, houseImage } = this
+
     return (
       <>
         <div>
-          <h1> SortingHat </h1>
+          <h2> SortingHat </h2>
         </div>
         <div className='hatbutton'>
-          <button onClick={this.handleButton}>Get Chosen</button>
+          <button onClick={handleButton}>Get Chosen</button>
         </div>
         <div className='chosenhouse'>
-          <p>{this.state.house}</p>
+          {houseImage()}
         </div>
       </>
     );
